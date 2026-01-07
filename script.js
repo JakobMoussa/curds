5d271721e5d2c5e3d1d752ee3632aa6c25a44977
jakob <jakobmoussa5@gmail.com>
2026-01-07 13:33:45 +0100

    Save data in local Storage

---
+++ script.js
@@ -0,0 +1,46 @@
+let title = document.getElementById('title');
+let price = document.getElementById('price');
+let taxes = document.getElementById('taxes');
+let ads = document.getElementById('ads');
+let discount = document.getElementById('discount');
+let total = document.getElementById('total');
+let count = document.getElementById('count');
+let category = document.getElementById('category');
+let submit = document.getElementById('submit');
+
+
+function getTotal() {
+    if (price.value != '') {
+        let result = (+price.value + +taxes.value + +ads.value) 
+        - +discount.value;
+        total.innerHTML = result;
+        total.style.background = '#040';
+    } else {
+        total.innerHTML = '';
+        total.style.background = '#a00d02';
+    }
+    
+}
+let dataPro;
+
+if(localStorage.product != null) {
+    dataPro = JSON.parse(localStorage.product)
+
+} else {
+    dataPro = [];
+}
+
+submit.onclick = function() {
+    let newPro = {
+        title:title.value,
+        price:price.value,
+        taxes:taxes.value,
+        ads:ads.value,
+        discount:discount.value,
+        total:total.innerHTML,
+        count:count.value,
+        category:category.value
+    }
+    dataPro.push(newPro);
+    localStorage.setItem('product',  JSON.stringify(dataPro))
+}
